d2c398c22f55b113e26840e3d862560274b88b05
Marius Ungureanu <therzok@gmail.com>
2019-10-24 11:04:04 +0300

    Fix package being busted

--- build.sh
+++ build.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 
+CONFIG=Release
+
 # Check the project builds and the projects which use the package build
-dotnet build -c Release NullabilityCompat.sln
-dotnet build -c Release NullabilityCompatTest.sln
+# dotnet build -c $CONFIG NullabilityCompat.sln
+# dotnet build -c $CONFIG NullabilityCompatTest.sln
 
 # Also build on Mono matrix
-msbuild /restore /p:Configuration=Release NullabilityCompat.sln
-msbuild /restore /p:Configuration=Release NullabilityCompatTest.sln
+msbuild /restore /p:Configuration=$CONFIG NullabilityCompat.sln
+msbuild /restore /p:Configuration=$CONFIG NullabilityCompatTest.sln
+
